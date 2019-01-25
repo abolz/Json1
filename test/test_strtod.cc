0edefdd08c3dc4ded47c840e0625ecd82c00e9f9
Alexander Bolz <lt.morris.schaffer@googlemail.com>
2019-01-25 19:12:57 +0100

    Update tests

--- test/test_strtod.cc
+++ test/test_strtod.cc
@@ -13,138 +13,162 @@ static inline double Strtod(std::string const& digits, int exponent)
 
 TEST_CASE("Strtod - double_conversion")
 {
-    constexpr double Inf = std::numeric_limits<double>::infinity();
-
     // Test cases from:
     // https://github.com/google/double-conversion/blob/master/test/cctest/test-strtod.cc
 
     char const* vector = "0";
-    CHECK_EQ(0.0, Strtod(vector, 1));
-    CHECK_EQ(0.0, Strtod(vector, 2));
-    CHECK_EQ(0.0, Strtod(vector, -2));
-    CHECK_EQ(0.0, Strtod(vector, -999));
-    CHECK_EQ(0.0, Strtod(vector, 999));
+    SECTION("0")
+    {
+        CHECK_EQ(0.0, Strtod(vector, 1));
+        CHECK_EQ(0.0, Strtod(vector, 2));
+        CHECK_EQ(0.0, Strtod(vector, -2));
+        CHECK_EQ(0.0, Strtod(vector, -999));
+        CHECK_EQ(0.0, Strtod(vector, 999));
+    }
 
     vector = "1";
-    CHECK_EQ(1.0, Strtod(vector, 0));
-    CHECK_EQ(10.0, Strtod(vector, 1));
-    CHECK_EQ(100.0, Strtod(vector, 2));
-    CHECK_EQ(1e20, Strtod(vector, 20));
-    CHECK_EQ(1e22, Strtod(vector, 22));
-    CHECK_EQ(1e23, Strtod(vector, 23));
-    CHECK_EQ(1e35, Strtod(vector, 35));
-    CHECK_EQ(1e36, Strtod(vector, 36));
-    CHECK_EQ(1e37, Strtod(vector, 37));
-    CHECK_EQ(1e-1, Strtod(vector, -1));
-    CHECK_EQ(1e-2, Strtod(vector, -2));
-    CHECK_EQ(1e-5, Strtod(vector, -5));
-    CHECK_EQ(1e-20, Strtod(vector, -20));
-    CHECK_EQ(1e-22, Strtod(vector, -22));
-    CHECK_EQ(1e-23, Strtod(vector, -23));
-    CHECK_EQ(1e-25, Strtod(vector, -25));
-    CHECK_EQ(1e-39, Strtod(vector, -39));
+    SECTION("1")
+    {
+        CHECK_EQ(1.0, Strtod(vector, 0));
+        CHECK_EQ(10.0, Strtod(vector, 1));
+        CHECK_EQ(100.0, Strtod(vector, 2));
+        CHECK_EQ(1e20, Strtod(vector, 20));
+        CHECK_EQ(1e22, Strtod(vector, 22));
+        CHECK_EQ(1e23, Strtod(vector, 23));
+        CHECK_EQ(1e35, Strtod(vector, 35));
+        CHECK_EQ(1e36, Strtod(vector, 36));
+        CHECK_EQ(1e37, Strtod(vector, 37));
+        CHECK_EQ(1e-1, Strtod(vector, -1));
+        CHECK_EQ(1e-2, Strtod(vector, -2));
+        CHECK_EQ(1e-5, Strtod(vector, -5));
+        CHECK_EQ(1e-20, Strtod(vector, -20));
+        CHECK_EQ(1e-22, Strtod(vector, -22));
+        CHECK_EQ(1e-23, Strtod(vector, -23));
+        CHECK_EQ(1e-25, Strtod(vector, -25));
+        CHECK_EQ(1e-39, Strtod(vector, -39));
+    }
 
     vector = "2";
-    CHECK_EQ(2.0, Strtod(vector, 0));
-    CHECK_EQ(20.0, Strtod(vector, 1));
-    CHECK_EQ(200.0, Strtod(vector, 2));
-    CHECK_EQ(2e20, Strtod(vector, 20));
-    CHECK_EQ(2e22, Strtod(vector, 22));
-    CHECK_EQ(2e23, Strtod(vector, 23));
-    CHECK_EQ(2e35, Strtod(vector, 35));
-    CHECK_EQ(2e36, Strtod(vector, 36));
-    CHECK_EQ(2e37, Strtod(vector, 37));
-    CHECK_EQ(2e-1, Strtod(vector, -1));
-    CHECK_EQ(2e-2, Strtod(vector, -2));
-    CHECK_EQ(2e-5, Strtod(vector, -5));
-    CHECK_EQ(2e-20, Strtod(vector, -20));
-    CHECK_EQ(2e-22, Strtod(vector, -22));
-    CHECK_EQ(2e-23, Strtod(vector, -23));
-    CHECK_EQ(2e-25, Strtod(vector, -25));
-    CHECK_EQ(2e-39, Strtod(vector, -39));
+    SECTION("2")
+    {
+        CHECK_EQ(2.0, Strtod(vector, 0));
+        CHECK_EQ(20.0, Strtod(vector, 1));
+        CHECK_EQ(200.0, Strtod(vector, 2));
+        CHECK_EQ(2e20, Strtod(vector, 20));
+        CHECK_EQ(2e22, Strtod(vector, 22));
+        CHECK_EQ(2e23, Strtod(vector, 23));
+        CHECK_EQ(2e35, Strtod(vector, 35));
+        CHECK_EQ(2e36, Strtod(vector, 36));
+        CHECK_EQ(2e37, Strtod(vector, 37));
+        CHECK_EQ(2e-1, Strtod(vector, -1));
+        CHECK_EQ(2e-2, Strtod(vector, -2));
+        CHECK_EQ(2e-5, Strtod(vector, -5));
+        CHECK_EQ(2e-20, Strtod(vector, -20));
+        CHECK_EQ(2e-22, Strtod(vector, -22));
+        CHECK_EQ(2e-23, Strtod(vector, -23));
+        CHECK_EQ(2e-25, Strtod(vector, -25));
+        CHECK_EQ(2e-39, Strtod(vector, -39));
+    }
 
     vector = "9";
-    CHECK_EQ(9.0, Strtod(vector, 0));
-    CHECK_EQ(90.0, Strtod(vector, 1));
-    CHECK_EQ(900.0, Strtod(vector, 2));
-    CHECK_EQ(9e20, Strtod(vector, 20));
-    CHECK_EQ(9e22, Strtod(vector, 22));
-    CHECK_EQ(9e23, Strtod(vector, 23));
-    CHECK_EQ(9e35, Strtod(vector, 35));
-    CHECK_EQ(9e36, Strtod(vector, 36));
-    CHECK_EQ(9e37, Strtod(vector, 37));
-    CHECK_EQ(9e-1, Strtod(vector, -1));
-    CHECK_EQ(9e-2, Strtod(vector, -2));
-    CHECK_EQ(9e-5, Strtod(vector, -5));
-    CHECK_EQ(9e-20, Strtod(vector, -20));
-    CHECK_EQ(9e-22, Strtod(vector, -22));
-    CHECK_EQ(9e-23, Strtod(vector, -23));
-    CHECK_EQ(9e-25, Strtod(vector, -25));
-    CHECK_EQ(9e-39, Strtod(vector, -39));
+    SECTION("9")
+    {
+        CHECK_EQ(9.0, Strtod(vector, 0));
+        CHECK_EQ(90.0, Strtod(vector, 1));
+        CHECK_EQ(900.0, Strtod(vector, 2));
+        CHECK_EQ(9e20, Strtod(vector, 20));
+        CHECK_EQ(9e22, Strtod(vector, 22));
+        CHECK_EQ(9e23, Strtod(vector, 23));
+        CHECK_EQ(9e35, Strtod(vector, 35));
+        CHECK_EQ(9e36, Strtod(vector, 36));
+        CHECK_EQ(9e37, Strtod(vector, 37));
+        CHECK_EQ(9e-1, Strtod(vector, -1));
+        CHECK_EQ(9e-2, Strtod(vector, -2));
+        CHECK_EQ(9e-5, Strtod(vector, -5));
+        CHECK_EQ(9e-20, Strtod(vector, -20));
+        CHECK_EQ(9e-22, Strtod(vector, -22));
+        CHECK_EQ(9e-23, Strtod(vector, -23));
+        CHECK_EQ(9e-25, Strtod(vector, -25));
+        CHECK_EQ(9e-39, Strtod(vector, -39));
+    }
 
     vector = "12345";
-    CHECK_EQ(12345.0, Strtod(vector, 0));
-    CHECK_EQ(123450.0, Strtod(vector, 1));
-    CHECK_EQ(1234500.0, Strtod(vector, 2));
-    CHECK_EQ(12345e20, Strtod(vector, 20));
-    CHECK_EQ(12345e22, Strtod(vector, 22));
-    CHECK_EQ(12345e23, Strtod(vector, 23));
-    CHECK_EQ(12345e30, Strtod(vector, 30));
-    CHECK_EQ(12345e31, Strtod(vector, 31));
-    CHECK_EQ(12345e32, Strtod(vector, 32));
-    CHECK_EQ(12345e35, Strtod(vector, 35));
-    CHECK_EQ(12345e36, Strtod(vector, 36));
-    CHECK_EQ(12345e37, Strtod(vector, 37));
-    CHECK_EQ(12345e-1, Strtod(vector, -1));
-    CHECK_EQ(12345e-2, Strtod(vector, -2));
-    CHECK_EQ(12345e-5, Strtod(vector, -5));
-    CHECK_EQ(12345e-20, Strtod(vector, -20));
-    CHECK_EQ(12345e-22, Strtod(vector, -22));
-    CHECK_EQ(12345e-23, Strtod(vector, -23));
-    CHECK_EQ(12345e-25, Strtod(vector, -25));
-    CHECK_EQ(12345e-39, Strtod(vector, -39));
+    SECTION("12345")
+    {
+        CHECK_EQ(12345.0, Strtod(vector, 0));
+        CHECK_EQ(123450.0, Strtod(vector, 1));
+        CHECK_EQ(1234500.0, Strtod(vector, 2));
+        CHECK_EQ(12345e20, Strtod(vector, 20));
+        CHECK_EQ(12345e22, Strtod(vector, 22));
+        CHECK_EQ(12345e23, Strtod(vector, 23));
+        CHECK_EQ(12345e30, Strtod(vector, 30));
+        CHECK_EQ(12345e31, Strtod(vector, 31));
+        CHECK_EQ(12345e32, Strtod(vector, 32));
+        CHECK_EQ(12345e35, Strtod(vector, 35));
+        CHECK_EQ(12345e36, Strtod(vector, 36));
+        CHECK_EQ(12345e37, Strtod(vector, 37));
+        CHECK_EQ(12345e-1, Strtod(vector, -1));
+        CHECK_EQ(12345e-2, Strtod(vector, -2));
+        CHECK_EQ(12345e-5, Strtod(vector, -5));
+        CHECK_EQ(12345e-20, Strtod(vector, -20));
+        CHECK_EQ(12345e-22, Strtod(vector, -22));
+        CHECK_EQ(12345e-23, Strtod(vector, -23));
+        CHECK_EQ(12345e-25, Strtod(vector, -25));
+        CHECK_EQ(12345e-39, Strtod(vector, -39));
+    }
 
     vector = "12345678901234";
-    CHECK_EQ(12345678901234.0, Strtod(vector, 0));
-    CHECK_EQ(123456789012340.0, Strtod(vector, 1));
-    CHECK_EQ(1234567890123400.0, Strtod(vector, 2));
-    CHECK_EQ(12345678901234e20, Strtod(vector, 20));
-    CHECK_EQ(12345678901234e22, Strtod(vector, 22));
-    CHECK_EQ(12345678901234e23, Strtod(vector, 23));
-    CHECK_EQ(12345678901234e30, Strtod(vector, 30));
-    CHECK_EQ(12345678901234e31, Strtod(vector, 31));
-    CHECK_EQ(12345678901234e32, Strtod(vector, 32));
-    CHECK_EQ(12345678901234e35, Strtod(vector, 35));
-    CHECK_EQ(12345678901234e36, Strtod(vector, 36));
-    CHECK_EQ(12345678901234e37, Strtod(vector, 37));
-    CHECK_EQ(12345678901234e-1, Strtod(vector, -1));
-    CHECK_EQ(12345678901234e-2, Strtod(vector, -2));
-    CHECK_EQ(12345678901234e-5, Strtod(vector, -5));
-    CHECK_EQ(12345678901234e-20, Strtod(vector, -20));
-    CHECK_EQ(12345678901234e-22, Strtod(vector, -22));
-    CHECK_EQ(12345678901234e-23, Strtod(vector, -23));
-    CHECK_EQ(12345678901234e-25, Strtod(vector, -25));
-    CHECK_EQ(12345678901234e-39, Strtod(vector, -39));
+    SECTION("12345678901234")
+    {
+        CHECK_EQ(12345678901234.0, Strtod(vector, 0));
+        CHECK_EQ(123456789012340.0, Strtod(vector, 1));
+        CHECK_EQ(1234567890123400.0, Strtod(vector, 2));
+        CHECK_EQ(12345678901234e20, Strtod(vector, 20));
+        CHECK_EQ(12345678901234e22, Strtod(vector, 22));
+        CHECK_EQ(12345678901234e23, Strtod(vector, 23));
+        CHECK_EQ(12345678901234e30, Strtod(vector, 30));
+        CHECK_EQ(12345678901234e31, Strtod(vector, 31));
+        CHECK_EQ(12345678901234e32, Strtod(vector, 32));
+        CHECK_EQ(12345678901234e35, Strtod(vector, 35));
+        CHECK_EQ(12345678901234e36, Strtod(vector, 36));
+        CHECK_EQ(12345678901234e37, Strtod(vector, 37));
+        CHECK_EQ(12345678901234e-1, Strtod(vector, -1));
+        CHECK_EQ(12345678901234e-2, Strtod(vector, -2));
+        CHECK_EQ(12345678901234e-5, Strtod(vector, -5));
+        CHECK_EQ(12345678901234e-20, Strtod(vector, -20));
+        CHECK_EQ(12345678901234e-22, Strtod(vector, -22));
+        CHECK_EQ(12345678901234e-23, Strtod(vector, -23));
+        CHECK_EQ(12345678901234e-25, Strtod(vector, -25));
+        CHECK_EQ(12345678901234e-39, Strtod(vector, -39));
+    }
 
     vector = "123456789012345";
-    CHECK_EQ(123456789012345.0, Strtod(vector, 0));
-    CHECK_EQ(1234567890123450.0, Strtod(vector, 1));
-    CHECK_EQ(12345678901234500.0, Strtod(vector, 2));
-    CHECK_EQ(123456789012345e20, Strtod(vector, 20));
-    CHECK_EQ(123456789012345e22, Strtod(vector, 22));
-    CHECK_EQ(123456789012345e23, Strtod(vector, 23));
-    CHECK_EQ(123456789012345e35, Strtod(vector, 35));
-    CHECK_EQ(123456789012345e36, Strtod(vector, 36));
-    CHECK_EQ(123456789012345e37, Strtod(vector, 37));
-    CHECK_EQ(123456789012345e39, Strtod(vector, 39));
-    CHECK_EQ(123456789012345e-1, Strtod(vector, -1));
-    CHECK_EQ(123456789012345e-2, Strtod(vector, -2));
-    CHECK_EQ(123456789012345e-5, Strtod(vector, -5));
-    CHECK_EQ(123456789012345e-20, Strtod(vector, -20));
-    CHECK_EQ(123456789012345e-22, Strtod(vector, -22));
-    CHECK_EQ(123456789012345e-23, Strtod(vector, -23));
-    CHECK_EQ(123456789012345e-25, Strtod(vector, -25));
-    CHECK_EQ(123456789012345e-39, Strtod(vector, -39));
+    SECTION("123456789012345")
+    {
+        CHECK_EQ(123456789012345.0, Strtod(vector, 0));
+        CHECK_EQ(1234567890123450.0, Strtod(vector, 1));
+        CHECK_EQ(12345678901234500.0, Strtod(vector, 2));
+        CHECK_EQ(123456789012345e20, Strtod(vector, 20));
+        CHECK_EQ(123456789012345e22, Strtod(vector, 22));
+        CHECK_EQ(123456789012345e23, Strtod(vector, 23));
+        CHECK_EQ(123456789012345e35, Strtod(vector, 35));
+        CHECK_EQ(123456789012345e36, Strtod(vector, 36));
+        CHECK_EQ(123456789012345e37, Strtod(vector, 37));
+        CHECK_EQ(123456789012345e39, Strtod(vector, 39));
+        CHECK_EQ(123456789012345e-1, Strtod(vector, -1));
+        CHECK_EQ(123456789012345e-2, Strtod(vector, -2));
+        CHECK_EQ(123456789012345e-5, Strtod(vector, -5));
+        CHECK_EQ(123456789012345e-20, Strtod(vector, -20));
+        CHECK_EQ(123456789012345e-22, Strtod(vector, -22));
+        CHECK_EQ(123456789012345e-23, Strtod(vector, -23));
+        CHECK_EQ(123456789012345e-25, Strtod(vector, -25));
+        CHECK_EQ(123456789012345e-39, Strtod(vector, -39));
+    }
+}
+
+TEST_CASE("Strtod - double_conversion - part 2")
+{
+    constexpr double Inf = std::numeric_limits<double>::infinity();
 
     CHECK_EQ(0.0,        Strtod("0", 12345));
     CHECK_EQ(0.0,        Strtod("", 1324));
